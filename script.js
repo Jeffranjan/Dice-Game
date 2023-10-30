71845c454fbe498aee71905bded033091d61b3c5
Ranjan Gupta <Ranjanguptajeff@gmail.com>
2023-10-30 14:06:11 +0530

    Renamed the HTML file

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 1 Wins! 🚩";
+  document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
 } else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
 } else {
   document.querySelector("h1").innerHTML = "Draw!";
   s;
